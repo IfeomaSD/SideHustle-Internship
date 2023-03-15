c81a88fa643f8dbe5621e6a3517cc7b8653932f2
IfeomaSD <buffie001@gmail.com>
2023-03-15 05:18:58 +0100

    Changed success message to alert, updated html and css for input types and gender

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 
-  var btn = document.querySelector('button');
-function changeColor () {
-btn.style.backgroundColor = 'pink';
-}
+//   var btn = document.querySelector('button');
+// function changeColor () {
+// btn.style.backgroundColor = 'green';
+// }
 
-const form = document.querySelector('form');
-const successMessage = document.querySelector('#form');
-form.addEventListener('submit', () => {
-    e.preventDefault();
-    successMessage.classList.add('show');
-    setTimeout(() => form.submit(), 2000);
-} );
+function formSubmit() {
+  alert ("Successful!");
+  form.reset();
+  }
